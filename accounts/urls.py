69513bc7face04f4076bb6296994693667405935
Anastasia Tuzhilina <haziran@live.ie>
2020-04-04 18:10:39 +0000

    Add Login functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import logout
+from .views import login, logout
 
 urlpatterns = [
     url(r'^logout/$', logout, name='logout'),
+    url(r'^login/$', login, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -7,7 +7,13 @@ def index(request):
     return render(request, 'index.html')
 
 
+# Log the user out
 def logout(request):
     auth.logout(request)
     messages.success(request, "You have been successfully logged out")
     return redirect(reverse('index'))
+
+
+# Return a login page
+def login(request):
+    return render(request, 'login.html')
