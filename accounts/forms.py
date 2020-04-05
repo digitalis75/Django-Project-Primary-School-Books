fd06fb2ca62f54d5759d30fbc6ac9c06c1c4b0bb
Anastasia Tuzhilina <haziran@live.ie>
2020-04-05 18:59:47 +0000

    Create the registration form with UserCreationForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,20 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Form to log in user
 class UserLoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+# Form to register a new user
+class UserRegistrationForm(UserCreationForm):
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Password Confirmation",
+                                widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password1', 'password2']
