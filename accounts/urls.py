3b40ae3304c4febf282621cea17f7f23e6b6001c
Anastasia Tuzhilina <haziran@live.ie>
2020-04-09 21:07:14 +0000

    URLs for password reset functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from .views import login, logout, registration
+from . import url_reset
 
 urlpatterns = [
     url(r'^logout/$', logout, name='logout'),
     url(r'^login/$', login, name='login'),
     url(r'^register/$', registration, name='registration'),
+    url(r'^password_reset/', include(url_reset))
 ]
