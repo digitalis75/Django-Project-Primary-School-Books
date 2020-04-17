828e0b50a44802742cc4e2dc2b9907aab4315f83
Anastasia Tuzhilina <haziran@live.ie>
2020-04-17 20:32:53 +0000

    Creating products model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+STATUS_CHOICES = (
+    ('IN_STOCK', 'In Stock'),
+    ('TEMP_OUT_OF_STOCK', 'Temporary Out of Stock'),
+)
+
+
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=254, default='')
+    author = models.CharField(max_length=254, default='')
+    school_level = models.CharField(max_length=40, default='')
+    stage = models.CharField(max_length=40, default='')
+    subject = models.CharField(max_length=40, default='')
+    publisher = models.CharField(max_length=50, default='')
+    product = models.CharField(max_length=20, default='')
+    cover_format = models.CharField(max_length=20, default='')
+    language = models.CharField(max_length=40, default='')
+    pages = models.IntegerField()
+    publication_year = models.IntegerField()
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='images')
+    status = models.CharField(max_length=40, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return self.name
