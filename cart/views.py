36b7dba80eee57487c285d012de2950320facd32
Anastasia Tuzhilina <haziran@live.ie>
2020-05-01 16:26:01 +0000

    Store shopping cart items in session

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+
 
 # Create your views here.
+# Renders the cart contents page
+def view_cart(request):
+    return render(request, "cart.html")
+
+
+# Add a quantity of the specified product to the cart
+def add_to_cart(request, id):
+    # quantity = int(request.POST.get('quantity'))
+    quantity = 1
+    cart = request.session.get('cart', {})
+    cart[id] = cart.get(id, quantity)
+
+    request.session['cart'] = cart
+    return redirect(reverse('index'))
+
+
+# Adjust the quantity of the specified product to the specified amount
+def adjust_cart(request, id):
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[id] = quantity
+    # else:
+    #     cart.pop(id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+# Delete cart item
+def delete_cart_item(request, id):
+    cart = request.session.get('cart', {})
+
+    cart.pop(id)
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
