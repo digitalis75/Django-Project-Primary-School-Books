b27243d6af742b6b05d0e4527e19f5741f6857aa
Anastasia Tuzhilina <haziran@live.ie>
2020-05-07 18:53:20 +0000

    Create product-info.html, its view and url

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import all_products
+from .views import all_products, product_info
 
 urlpatterns = [
     url(r'^$', all_products, name='products'),
+    url(r'^(?P<slug>[\w-]+)/$', product_info, name='product_info'),
 ]
--- products/views.py
+++ products/views.py
@@ -6,3 +6,8 @@ from .models import Product
 def all_products(request):
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
+
+
+def product_info(request, slug):
+    product = Product.objects.get(slug=slug)
+    return render(request, "product-info.html", {'product': product})
