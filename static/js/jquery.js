185e229d843a7ee287b6a9a72672e54d1e942764
Anastasia Tuzhilina <haziran@live.ie>
2020-05-24 15:07:49 +0000

    Adding text search for mobile devices

---
+++ static/js/jquery.js
@@ -0,0 +1,9 @@
+ $(document).ready(function(){
+
+    // Hide and toggle search bar on mobile devives
+    $(".search-bar1").hide();
+    $(".search-icon").click(function(){
+        $(".search-bar1").toggle();
+    });
+
+});
