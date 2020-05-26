731f04bd0a089f23e360514e59f9cecdfa3c22e8
Anastasia Tuzhilina <haziran@live.ie>
2020-05-26 10:07:15 +0000

    Added navbar dropdown menu with search by class

--- search/urls.py
+++ search/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import do_search
+from .views import do_search, search_by_class
 
 urlpatterns = [
     url(r'^$', do_search, name='search'),
+    url(r'^(?P<stage>[\w\-]+)/$', search_by_class, name='search_by_class')
 ]
--- search/views.py
+++ search/views.py
@@ -6,3 +6,8 @@ from products.models import Product
 def do_search(request):
     products = Product.objects.filter(name__icontains=request.GET['q'])
     return render(request, "search_results.html", {"products": products})
+
+
+def search_by_class(request, stage):
+    products = Product.objects.filter(stage=stage)
+    return render(request, "search_results.html", {"products": products, "stage": stage})
