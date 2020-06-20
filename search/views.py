1873213dae3ebe5b6a9932224c380fd73c8adad5
Anastasia Tuzhilina <haziran@live.ie>
2020-06-20 21:58:22 +0000

    Add pagination, total number of results for search_results page. Create search_by_class_subject.html

--- search/views.py
+++ search/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from products.models import Product
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
 def do_search(request):
-    products = Product.objects.filter(name__icontains=request.GET['q'])
-    return render(request, "search_results.html", {"products": products})
+    product_list = Product.objects.filter(name__icontains=request.GET['q'])
+    q = request.GET['q']
+    page = request.GET.get('page', 1)
+    paginator = Paginator(product_list, 12)
+    total_products = product_list.count()
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+    return render(request, "search_results.html", {"products": products,
+                  "total_products": total_products, "q": q})
 
 
 def search_by_class(request, stage):
@@ -15,6 +28,15 @@ def search_by_class(request, stage):
 
 
 def search_by_class_subject(request, stage, subject):
-    products = Product.objects.filter(stage=stage).filter(subject=subject)
-    return render(request, "search_results.html", {"products": products,
-                  "stage": stage, "subject": subject})
+    product_list = Product.objects.filter(stage=stage).filter(subject=subject)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(product_list, 12)
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+    return render(request, "search_by_class_subject.html",
+                  {"products": products,
+                   "stage": stage, "subject": subject})
