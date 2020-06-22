cc00a43f9e567960b466fbbf2455f3e37f6151f0
Anastasia Tuzhilina <haziran@live.ie>
2020-06-22 20:32:00 +0000

    Add templates for resetting password

--- estore/settings.py
+++ estore/settings.py
@@ -181,3 +181,9 @@ MESSAGE_TAGS = {
     messages.WARNING: 'alert-warning',
     messages.ERROR: 'alert-danger',
 }
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
